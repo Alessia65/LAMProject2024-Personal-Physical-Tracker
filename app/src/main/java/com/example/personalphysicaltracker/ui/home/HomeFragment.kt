61449c640a3f5a2f2b739848529acb26dd4ca7a5
Alessia65 <alessia.disabato@studio.unibo.it>
2024-06-14 18:20:02 +0200

    base per la progressive bar

--- app/src/main/java/com/example/personalphysicaltracker/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/personalphysicaltracker/ui/home/HomeFragment.kt
@@ -5,19 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.appcompat.app.AlertDialog
-import com.example.mvvm_todolist.TrackingRepository
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.personalphysicaltracker.R
 import com.example.personalphysicaltracker.activities.AccelerometerListener
-import com.example.personalphysicaltracker.activities.Activity
 import com.example.personalphysicaltracker.activities.DrivingActivity
 import com.example.personalphysicaltracker.activities.StandingActivity
 import com.example.personalphysicaltracker.activities.WalkingActivity
-import com.example.personalphysicaltracker.database.ActivityViewModel
-import com.example.personalphysicaltracker.database.ActivityViewModelFactory
 import com.example.personalphysicaltracker.databinding.FragmentHomeBinding
 
 
@@ -30,8 +28,13 @@ class HomeFragment : Fragment(), AccelerometerListener {
 
     private lateinit var textView: TextView
     private lateinit var buttonStartActivity: Button
+    private lateinit var btn_decr: Button
+    private lateinit var btn_incr: Button
+    private lateinit var text_progress_bar: TextView
     private lateinit var accelText: TextView
+    private lateinit var progress_bar: ProgressBar
 
+    private var progressBar = 0
 
 
     override fun onCreateView(
@@ -48,14 +51,46 @@ class HomeFragment : Fragment(), AccelerometerListener {
         homeViewModel.initializeModel(this.activity, this)
 
         textView = binding.textHome
-        buttonStartActivity = root.findViewById(R.id.button_startActivity)
         accelText = root.findViewById(R.id.acceleration_text)
+        text_progress_bar = root.findViewById(R.id.text_progress_bar)
+        progress_bar = root.findViewById<ProgressBar>(R.id.progress_bar)
 
+        updateProgressBar()
+        initializeButtons(root)
+
+
+        return root
+    }
+
+    private fun initializeButtons(root: ConstraintLayout){
+        btn_decr = root.findViewById(R.id.btn_decr)
+        btn_incr = root.findViewById(R.id.btn_incr)
+
+        btn_decr.setOnClickListener(){
+            if (progressBar >= 5) {
+                progressBar -= 5
+                updateProgressBar()
+            }
+        }
+
+        btn_incr.setOnClickListener(){
+            if (progressBar <= 95) {
+                progressBar += 5
+                updateProgressBar()
+            }
+        }
+
+        buttonStartActivity = root.findViewById(R.id.button_startActivity)
         buttonStartActivity.setOnClickListener {
             showActivitySelectionDialog()
         }
 
-        return root
+    }
+
+    private fun updateProgressBar(){
+        progress_bar.progress = progressBar
+        text_progress_bar.text = progressBar.toString()
+
     }
 
     private fun showActivitySelectionDialog() {
