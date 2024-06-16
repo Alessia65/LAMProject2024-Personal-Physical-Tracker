7c49603966aeb55e69ed75ab3f854617375142aa
Alessia65 <alessia.disabato@studio.unibo.it>
2024-06-16 18:23:00 +0200

    correzione accellerometro

--- app/src/main/java/com/example/personalphysicaltracker/activities/SensorHandler.kt
+++ app/src/main/java/com/example/personalphysicaltracker/activities/SensorHandler.kt
@@ -17,7 +17,6 @@ class SensorHandler(private val context: Context) : SensorEventListener {
 
     private var sensorManager: SensorManager? = null
     private var accelerometerListener: AccelerometerListener? = null
-    private var sensorJob: Job? = null
 
     init {
         sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager?
@@ -34,33 +33,11 @@ class SensorHandler(private val context: Context) : SensorEventListener {
         accelerometerSensor?.let {
             sensorManager?.registerListener(this, it, SensorManager.SENSOR_DELAY_NORMAL)
         }
-
-        sensorJob = CoroutineScope(Dispatchers.IO).launch {
-            // Logica per avviare il sensore, eseguire operazioni periodiche, ecc.
-            while (isActive) {
-                // Esempio di lettura dei dati dell'accelerometro
-                // Simuliamo la ricezione di dati casuali per scopi di esempio
-                val accelX = (Math.random() * 10).toFloat()
-                val accelY = (Math.random() * 10).toFloat()
-                val accelZ = (Math.random() * 10).toFloat()
-                val message = "Dati: \n" +
-                        "Acc. X: ${String.format("%.2f", accelX)} \n" +
-                        "Acc. Y: ${String.format("%.2f", accelY)} \n" +
-                        "Acc. Z: ${String.format("%.2f", accelZ)}"
-
-                // Notifica il listener dell'accelerometro con i nuovi dati
-                accelerometerListener?.onAccelerometerDataReceived(message)
-
-                // Simuliamo un intervallo di 1 secondo tra le letture
-                kotlinx.coroutines.delay(1000)
-            }
-        }
     }
 
     // Metodo per fermare il sensore dell'accelerometro
     fun stopAccelerometer() {
         sensorManager?.unregisterListener(this)
-        sensorJob?.cancel()
     }
 
     // Metodo richiamato quando i dati del sensore cambiano
