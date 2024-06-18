016fd081b21c149e9fb81d0f7de2e37c83d57219
Alessia65 <alessia.disabato@studio.unibo.it>
2024-06-18 17:26:12 +0200

    Grafica calendario

--- app/src/main/java/com/example/personalphysicaltracker/ui/calendar/CalendarViewModel.kt
+++ app/src/main/java/com/example/personalphysicaltracker/ui/calendar/CalendarViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.personalphysicaltracker.ui.dashboard
+package com.example.personalphysicaltracker.ui.calendar
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class CalendarViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
